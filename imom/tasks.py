3367904b0bf413aa1362e0028e28012f62bd5315
Sonal Kumar <skbrahee@gmail.com>
2020-08-30 16:36:04 +0530

    Initial commit

---
+++ imom/tasks.py
@@ -0,0 +1,44 @@
+from __future__ import absolute_import, unicode_literals
+
+import ast
+import os
+import traceback
+from celery import shared_task
+from django.conf import settings
+from subprocess import Popen, PIPE
+from .models import Speaker, Meeting, Transcript
+from celery.utils.log import get_task_logger
+
+@shared_task
+def transcript_summary(mid):
+    meeting = Meeting.objects.get(pk=mid)
+    pipeline_path = os.path.join(settings.BASE_DIR, 'transcript','pipeline.py')
+    process = Popen(['python', pipeline_path, '--filename', meeting.audio.path], stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    print(stderr.decode('utf-8'))
+    output = stdout.decode('utf-8').strip()
+    try:
+        output = ast.literal_eval(output)
+    except:
+        traceback.print_exc()
+        output = {0 : [(0,"There was an error while generating the transcript")]}
+    for speaker in output.keys():
+        s = Speaker(name='speaker %d' % speaker, mid=meeting)
+        s.save()
+        for t in output[speaker]:
+            entry = Transcript(timestamp=t[0],text=t[1],sid=s,mid=meeting)
+            entry.save()
+
+    summarizer_path = os.path.join(settings.BASE_DIR, 'summarization.py')
+    process = Popen(['python', summarizer_path, '--filename', meeting.audio.path], stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    print(stderr.decode('utf-8'))
+    meeting.summary = stdout.decode('utf-8').strip()
+    meeting.save()
+
+    wavs = [os.path.join(settings.BASE_DIR,w) for w in os.listdir() if w.endswith('.wav')]
+    wavs += [os.path.join(settings.BASE_DIR, 'audio_chunks',w) for w in os.listdir('audio_chunks') if w.endswith('.wav')]
+
+    for wav in wavs:
+        os.remove(wav)
+    
---
+++ imom/views.py
@@ -0,0 +1,67 @@
+import os
+import ast
+import io
+from django.conf import settings
+from subprocess import Popen, PIPE
+from django.urls import reverse
+from django.views import View
+from django.http import FileResponse
+from django.shortcuts import render, redirect
+from .tasks import transcript_summary
+from .models import Meeting, MeetingForm, SpeakerEditForm, Speaker, Transcript
+
+# Create your views here.
+class Home(View):
+    form_class = MeetingForm
+    template_name = 'imom/home.html'
+
+    def get(self, request):
+        meetings = Meeting.objects.all()
+        form = self.form_class(auto_id=False)
+        return render(request, self.template_name, {"form":form, "meetings":meetings})
+    
+    def post(self, request):
+        form = self.form_class(request.POST, request.FILES)
+        if form.is_valid():
+            meeting = form.save(commit=False)
+            meeting.save()
+            transcript_summary.delay(meeting.id)
+            return redirect(reverse('imom:home'))
+        else:
+            return render(request, self.template_name, {"form": form})
+
+def transcript(request,mid):
+    form = SpeakerEditForm(mid=mid)
+    meeting = Meeting.objects.get(pk=mid)
+    transcripts = Transcript.objects.filter(mid=meeting)
+    if request.method == 'POST':
+        form = SpeakerEditForm(request.POST)
+        if form.is_valid():
+            sid = form.cleaned_data['sid'].id
+            new_name = form.cleaned_data['new_name']
+            speaker = Speaker.objects.get(pk=int(sid))
+            speaker.name = new_name
+            speaker.save()
+            return redirect(reverse('imom:transcript', args=(mid,)))
+        
+    return render(request, 'imom/transcript.html', {"form":form, "transcripts":transcripts, "meeting":meeting})
+
+def download_transcript(request, mid):
+    f = io.BytesIO()
+    meeting = Meeting.objects.get(pk=mid)
+    transcripts = Transcript.objects.filter(mid=meeting)
+    for transcript in transcripts:
+        f.write(str(transcript).encode('utf-8') + b'\n')
+    f.seek(0)
+    return FileResponse(f, as_attachment=True, filename="%s_transcript.txt" % meeting.name)
+
+def download_summary(request, mid):
+    f = io.BytesIO()
+    meeting = Meeting.objects.get(pk=mid)
+    f.write(meeting.summary.encode('utf-8')); f.seek(0)
+    return FileResponse(f, as_attachment=True, filename="%s_summary.txt" % meeting.name)
+
+def delete(request,mid):
+    meeting = Meeting.objects.get(pk=mid)
+    meeting.delete()
+    return redirect(reverse('imom:home'))
